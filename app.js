8eb78e71da06e9cabf339d3a3b4183c8d4df2047
Murali <muralikl312@gmail.com>
2026-01-30 15:09:13 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Hello from Node.js CI Pipeline! from jenkins ");
 console.log("GitHub user: Murali-312");
-console.log("Triggered by Jenkins Polling SCM testing from git.");
+console.log("Triggered by Jenkins Polling SCM testing from git......");
 
